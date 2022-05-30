1357ced4059132335b3ca05c3ff298bffe1392ac
Jeasine Ma <jeasinema@gmail.com>
2022-05-30 12:11:35 +0800

    [Add] headers

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+#
+# This work is licensed under the NVIDIA Source Code License
+# for Bongard-HOI. To view a copy of this license, see the LICENSE file.
+# ----------------------------------------------------------------------
+
 import os
 
 
